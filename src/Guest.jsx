f5a54fbee65879854d57cf926ad2129fca619cc0
Liz Verde <>
2017-12-10 16:17:30 +0000

    Setting up the Guest and GuestList jsx files

---
+++ src/Guest.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Guest = props => (
+		
+	<li className="responded">
+		<span>{ props.name }</span>
+		<label>
+			<input 
+				type="checkbox" 
+				checked={ props.isConfirmed } 
+				onChange={ props.handleConfirmation }
+			/> Confirmed
+		</label>
+		<button>edit</button>
+		<button>remove</button>
+	</li>	
+);
+
+
+
+export default Guest;
